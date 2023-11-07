22a958db5e0a3bcacb834c35bdf582711495a009
João Henrique <joaohenriquepereiramachado@gmail.com>
2023-11-07 19:29:47 -0300

    collision with the borders of the screen

--- core/src/com/mygdx/mechanictests/ship/Ship.java
+++ core/src/com/mygdx/mechanictests/ship/Ship.java
@@ -1,6 +1,5 @@
 package com.mygdx.mechanictests.ship;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
